8ea49a22a2fbd8f6d66916641d7126f7fdca15c5
LOGAN LEE <dlfjgkssk1@naver.com>
2020-05-25 19:10:31 +0900

    add comps to home story

--- src/stories/1-1-Home.stories.js
+++ src/stories/1-1-Home.stories.js
@@ -1,10 +1,56 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+
 import Navigation from 'components/Navigation';
+import ThemeSwitcher from 'components/ThemeSwitcher';
+
+import { Provider } from 'react-redux';
+import Home from 'container/Home';
+import store from 'store';
+import {
+	Home as HomeIcon,
+	Save,
+	CloudUpload,
+	KeyboardVoice,
+	Receipt,
+} from '@material-ui/icons';
 
 export default {
 	title: 'Home',
 	// component: Home,
 };
 
-export const Navigation_ = () => <Navigation />;
+const menus = [
+	{
+		name: 'Main',
+		text: 'Main',
+		color: 'secondary',
+		startIcon: <HomeIcon />,
+	},
+	{
+		name: 'Main',
+		text: 'Main',
+		color: 'secondary',
+		startIcon: <CloudUpload />,
+	},
+	{
+		name: 'News',
+		text: 'News',
+		color: 'default',
+		startIcon: <Receipt />,
+	},
+	{
+		name: 'Main',
+		text: 'Main',
+		color: 'primary',
+		startIcon: <KeyboardVoice />,
+	},
+];
+
+export const Navigation_ = () => <Navigation menus={menus} />;
+export const ThemeSwitcher_ = () => <ThemeSwitcher />;
+export const Home_ = () => (
+	<Provider store={store}>
+		<Home menus={menus} />
+	</Provider>
+);
