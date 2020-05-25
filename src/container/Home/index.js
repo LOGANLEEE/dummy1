1ad483f22aceb035c61ddb9d5d69625a89b4910b
LOGAN LEE <dlfjgkssk1@naver.com>
2020-05-25 19:10:19 +0900

    add Home Container

--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -1,21 +1,47 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
+import { Grid } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import Navigation from 'components/Navigation';
+import styled from 'styled-components';
+
 import { incrementAsync, selectHomeTest, MAIN_TEST } from './Slice';
 
-const Home = (props) => {
+const Home = ({ menus }) => {
 	const mainTest = useSelector(selectHomeTest);
 	const dispatch = useDispatch();
 
 	return (
-		<div>
-			<span>current Value : {mainTest}</span>
-			<button onClick={() => dispatch(incrementAsync(3))}>
-				fire async
-			</button>
-			<button onClick={() => dispatch(MAIN_TEST())}>fire</button>
-		</div>
+		<Wrapper>
+			<Grid
+				container
+				direction='column'
+				justify='center'
+				alignItems='center'
+			>
+				<Grid
+					className='header'
+					container
+					direction='row'
+					justify='center'
+					alignItems='center'
+				>
+					<Navigation menus={menus} />
+					<Navigation menus={menus} />
+					<Navigation menus={menus} />
+				</Grid>
+				<div className='body'>body</div>
+				<div className='foot'>foot</div>
+			</Grid>
+		</Wrapper>
 	);
 };
+
+const Wrapper = styled.div``;
+
+Home.propTypes = {};
+
+Home.defaultProps = {};
 
 export default Home;
